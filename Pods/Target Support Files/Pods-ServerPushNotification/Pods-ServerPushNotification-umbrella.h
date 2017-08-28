d1a77ed124e06701b5d3f615a5362389de4162fe
Amir J <amir.jahan@gmail.com>
2017-08-28 11:20:55 -0700

    two

---
+++ Pods/Target Support Files/Pods-ServerPushNotification/Pods-ServerPushNotification-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_ServerPushNotificationVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ServerPushNotificationVersionString[];
+
